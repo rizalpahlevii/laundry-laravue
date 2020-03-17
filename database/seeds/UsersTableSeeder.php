799a3f54a69160a2d4468d247dae806389ed5932
Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2020-03-17 19:28:36 +0700

    update readme.md

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Outlet;
 use App\User;
 use Illuminate\Database\Seeder;
 
@@ -13,12 +14,39 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'Muhammad Rizal Pahlevi',
-            'email' => 'mrizalpahlevi372@gmail.com',
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
             'email_verified_at' => now(),
-            'password' => bcrypt('secret'),
+            'password' => bcrypt('12345678'),
             'role' => 0
         ]);
-        factory('App\User', 10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('12345678'),
+            'role' => 0
+        ]);
+        User::create([
+            'name' => 'Finance',
+            'email' => 'finance@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('12345678'),
+            'role' => 0
+        ]);
+        $outlet = Outlet::create([
+            'code' =>'KT6',
+            'name'=>'Jaya Raya',
+            'status'=>'Jalan Mojo lembah rt 3 rw 2',
+            'phone'=>'087721163128'
+        ]);
+        User::create([
+            'name' => 'kurir',
+            'email' => 'kurir@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('12345678'),
+            'role' => 0,
+            'outlet_id'=> $outlet->id
+        ]);
     }
 }
